7a5c8ae90c2b2b4a77cf4f384bdc5ed347b78385
Tye <tye.hickman89@gmail.com>
2021-03-25 21:57:16 -0500

    Added ionic toast component from Angular for Ionic video

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  title = "Grocery";
 
-  constructor() {}
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Cherries",
+      quantity: 10
+    },
+    {
+      name: "Yogurt",
+      quantity: 3
+    },
+    {
+      name: "Spinach",
+      quantity: 2
+    }
+  ];
+  constructor(public toastController: ToastController) {}
+
+  async removeItem(item) {
+    console.log("Removing Item... ", item);
+    const toast = await this.toastController.create({
+      // position: 'top',
+      message: 'Removing Item - ' + item.name + '...',
+      duration: 3000
+    });
+    toast.present();
+  }
 
 }
