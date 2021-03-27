079d0ff576132cac5d671565069587c33c65d7f6
Tye <tye.hickman89@gmail.com>
2021-03-26 21:58:48 -0500

    Added add and remove item functionality in accordance with the class videos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -28,16 +29,61 @@ export class Tab1Page {
       quantity: 2
     }
   ];
-  constructor(public toastController: ToastController) {}
+  constructor(public toastController: ToastController, public alertController: AlertController) {}
 
-  async removeItem(item) {
-    console.log("Removing Item... ", item);
+  async removeItem(item, index) {
+    console.log("Removing Item... ", item, index);
     const toast = await this.toastController.create({
       // position: 'top',
-      message: 'Removing Item - ' + item.name + '...',
+      message: 'Removing Item - ' + index + '...',
       duration: 3000
     });
     toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    console.log("Addding Item...");
+    this.showAddItemPrompt();
+  }
+
+  async showAddItemPrompt() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Item Name'
+        },
+        {
+          name: 'quantity',
+          type: 'number',
+          placeholder: '1'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'primary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Save',
+          handler: (item) => {
+            console.log('Confirm Ok', item);
+            this.items.push(item)
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
